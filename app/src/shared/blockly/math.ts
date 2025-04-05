af814255e3012144d86a046388313c5fc5b7beec
Tyler Holewinski <tyler@holewinski.dev>
2025-04-04 23:15:31 -0400

    feat: math blocks

--- app/src/shared/blockly/math.ts
+++ app/src/shared/blockly/math.ts
@@ -32,3 +32,58 @@ block("math_sub")
     .inline()
     .outputs("native-num")
     .impl(({ resolve }) => ({ value: `${resolve("lhs")}-${resolve("rhs")}`, order: ord.SUBTRACTION }));
+
+block("math_mul")
+    .meta("category", "Math")
+    .meta("shadow", shadows)
+    .slot("lhs", { allow: "native-num", content: (v) => v })
+    .slot("rhs", { allow: "native-num", content: (v) => v.text("*") })
+    .inline()
+    .outputs("native-num")
+    .impl(({ resolve }) => ({ value: `${resolve("lhs")}*${resolve("rhs")}`, order: ord.MULTIPLICATION }));
+
+block("math_div")
+    .meta("category", "Math")
+    .meta("shadow", shadows)
+    .slot("lhs", { allow: "native-num", content: (v) => v })
+    .slot("rhs", { allow: "native-num", content: (v) => v.text("/") })
+    .inline()
+    .outputs("native-num")
+    .impl(({ resolve }) => ({ value: `${resolve("lhs")}/${resolve("rhs")}`, order: ord.DIVISION }));
+
+block("math_pow")
+    .meta("category", "Math")
+    .meta("shadow", {
+        rhs: {
+            blockType: "val_num",
+            fields: {
+                value: 2,
+            },
+        },
+        lhs: {
+            blockType: "val_num",
+            fields: {
+                value: 3,
+            },
+        },
+    })
+    .slot("lhs", { allow: "native-num", content: (v) => v })
+    .slot("rhs", { allow: "native-num", content: (v) => v.text("to the power of") })
+    .inline()
+    .outputs("native-num")
+    .impl(({ resolve }) => ({ value: `${resolve("lhs")}^${resolve("rhs")}`, order: ord.EXPONENTIATION }));
+
+block("math_sqrt")
+    .meta("category", "Math")
+    .meta("shadow", {
+        val: {
+            blockType: "val_num",
+            fields: {
+                value: 25,
+            },
+        },
+    })
+    .slot("val", { allow: "native-num", content: (v) => v.text("√") })
+    .inline()
+    .outputs("native-num")
+    .impl(({ resolve }) => ({ value: `[root]^2(${resolve("val")})`, order: ord.EXPONENTIATION }));
