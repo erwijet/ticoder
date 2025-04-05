a749f1535d6229a88902e24e465c14d6d72987c3
Tyler Holewinski <tyler@holewinski.dev>
2025-04-04 23:23:25 -0400

    more stuff idk

--- app/src/shared/blockly/val.ts
+++ app/src/shared/blockly/val.ts
@@ -10,6 +10,26 @@ block("val_num")
     .outputs("native-num")
     .impl(({ fields }) => `${fields.value}`);
 
+block("var_num")
+    .meta("category", "Math")
+    .content((v) => v.variable("var", { types: ["native-num"] }))
+    .outputs("native-num")
+    .impl(({ fields }) => fields.var);
+
+block("io_input_num")
+    .meta("category", "Math")
+    .content((v) =>
+        v
+            .text("ask “")
+            .textbox("prompt", "value?")
+            .text("” for number ")
+            .variable("var", { types: ["native-num"] }),
+    )
+    .impl(({ fields }) => {
+        if (fields.prompt.toLowerCase() == fields.var.toLowerCase() + "?") return "Prompt " + fields.var;
+        return `Input \"${fields.prompt + " "}\",${fields.var}`;
+    });
+
 block("val_str")
     .meta("category", "Values")
     .content((v) => v.text("“").textbox("value", "text").text("”"))
